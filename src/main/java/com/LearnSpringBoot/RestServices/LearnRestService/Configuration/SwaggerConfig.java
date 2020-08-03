d47016fa34039384ec188aad670724cf67d1f433
Emanual Alby <emanual.alby@gmail.com>
2020-08-03 16:13:30 +0100

    Used ApiModel Annotation

--- src/main/java/com/LearnSpringBoot/RestServices/LearnRestService/Configuration/SwaggerConfig.java
+++ src/main/java/com/LearnSpringBoot/RestServices/LearnRestService/Configuration/SwaggerConfig.java
@@ -2,23 +2,47 @@ package com.LearnSpringBoot.RestServices.LearnRestService.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 public class SwaggerConfig {
 
+    public static final Contact DEFAULT_GENERATED_CONTACT
+            = new Contact(
+            "Emanual Alby",
+            "http://www.LearnSpring.com",
+            "emanual.alby@gmail.com");
+    
+    public static final ApiInfo DEFAULT_API_INFO
+            = new ApiInfo(
+            "Wonderful Lean springboot title",
+            "Wonderful Lean springboot title",
+            "1.0",
+            "urn:tos",
+            DEFAULT_GENERATED_CONTACT,
+            "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0",
+            new ArrayList<>());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES
+            = new HashSet<String>(Arrays.asList("application/XML", "application/JSON"));
+
     @Bean
     public Docket swaggerDocket(){
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(new ApiInfoBuilder()
-                        .description("Accept User")
-                        .title("Accept User")
-                        .version("1.0.0")
-                        .build())
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
                 .enable(true)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.LearnSpringBoot.RestServices.LearnRestService.api"))
--- src/main/java/com/LearnSpringBoot/RestServices/LearnRestService/Entity/User.java
+++ src/main/java/com/LearnSpringBoot/RestServices/LearnRestService/Entity/User.java
@@ -1,20 +1,25 @@
 package com.LearnSpringBoot.RestServices.LearnRestService.Entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "Required field to create a User")
 @Data
 public class User {
 
 
     private Integer userId;
 
+    @ApiModelProperty(notes = "Name should be at least 2 characters long")
     @Size(min =2, message = "Name of the user should be at least 2 characters")
     private String name;
 
+    @ApiModelProperty(notes = "Birth data cannot be in the past")
     @Past
     private Date birthDate;
 
